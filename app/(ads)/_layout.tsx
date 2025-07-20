c6c36c57d8d2eeeb045ce69d2faf807af7a78879
Eric Mensah <erecox.ceo@gmail.com>
2025-07-20 16:57:36 +0000

    few changes for ios

--- app/(ads)/_layout.tsx
+++ app/(ads)/_layout.tsx
@@ -4,29 +4,30 @@ import { SafeAreaView } from "@/components/ui/safe-area-view";
 import { Text } from "@/components/ui/text";
 import { router, Stack } from "expo-router";
 import React from "react";
+import { Platform } from "react-native";
 
 export default function AdsLayout() {
   return (
     <SafeAreaView allowBottom>
       <Stack
         screenOptions={{
-          headerLeft(props) {
-            return (
-              <Pressable
-                onPress={() => props.canGoBack && router.back()}
-                className="flex flex-row items-center"
-              >
-                <Icon size="2xl" as={ChevronLeftIcon} />
-                <Text size="lg">{props.label}</Text>
-              </Pressable>
-            );
-          },
+          headerLeft:
+            Platform.OS === "ios"
+              ? (props) => {
+                  return (
+                    <Pressable
+                      onPress={() => props.canGoBack && router.back()}
+                      className="flex flex-row items-center"
+                    >
+                      <Icon size="2xl" as={ChevronLeftIcon} />
+                      <Text size="lg">{props.label}</Text>
+                    </Pressable>
+                  );
+                }
+              : undefined,
         }}
       >
-        <Stack.Screen
-          name="[id]"
-          options={{ headerBackVisible: true, headerBackTitle: "Back" }}
-        />
+        <Stack.Screen name="[id]" />
         <Stack.Screen name="edit" />
         <Stack.Screen name="new" />
         <Stack.Screen
